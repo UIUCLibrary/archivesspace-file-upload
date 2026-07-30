8341e7116b895319c7f44cd2c59d44f793b7cfba
Leonel Ramirez <50677216+leonelramirez@users.noreply.github.com>
2026-07-30 13:43:42 -0500

    fix: updated for ArchivesSpace v4.0.0

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -6,23 +6,25 @@ Rails.application.config.after_initialize do
     def create
       handle_crud_with_file_uploads(:instance => :digital_object_component,
                   :find_opts => find_opts,
-                  :on_invalid => ->() { render_aspace_partial :partial => "new_inline" },
+                  :on_invalid => ->() { return render_aspace_partial :partial => "new_inline" },
                   :on_valid => ->(id) {
                     # Refetch the record to ensure all sub records are resolved
                     # (this object isn't marked as stale upon create like Archival Objects,
                     # so need to do it manually)
                     @digital_object_component = JSONModel(:digital_object_component).find(id, find_opts)
+                    digital_object = @digital_object_component['digital_object']['_resolved']
+                    parent = @digital_object_component['parent']? @digital_object_component['parent']['_resolved'] : false
 
                     flash[:success] = @digital_object_component.parent ?
-                      I18n.t("digital_object_component._frontend.messages.created_with_parent", JSONModelI18nWrapper.new(:digital_object_component => @digital_object_component, :digital_object => @digital_object_component['digital_object']['_resolved'], :parent => @digital_object_component['parent']['_resolved']).enable_parse_mixed_content!(url_for(:root))) :
-                      I18n.t("digital_object_component._frontend.messages.created", JSONModelI18nWrapper.new(:digital_object_component => @digital_object_component, :digital_object => @digital_object_component['digital_object']['_resolved']).enable_parse_mixed_content!(url_for(:root)))
+                      t("digital_object_component._frontend.messages.created_with_parent", digital_object_component_display_string: @digital_object_component.title, digital_object_title: digital_object['title'], parent_display_string: parent['title']) :
+                      t("digital_object_component._frontend.messages.created", digital_object_component_display_string: @digital_object_component.title, digital_object_title: digital_object['title'])
 
                     if @digital_object_component["is_slug_auto"] == false &&
                       @digital_object_component["slug"] == nil &&
                       params["digital_object_component"] &&
                       params["digital_object_component"]["is_slug_auto"] == "1"
 
-                      flash[:warning] = I18n.t("slug.autogen_disabled")
+                      flash[:warning] = t("slug.autogen_disabled")
                     end
 
                     render_aspace_partial :partial => "digital_object_components/edit_inline"
@@ -41,16 +43,16 @@ Rails.application.config.after_initialize do
                   :obj => @digital_object_component,
                   :on_invalid => ->() { return render_aspace_partial :partial => "edit_inline" },
                   :on_valid => ->(id) {
-                    flash.now[:success] = parent ?
-                      I18n.t("digital_object_component._frontend.messages.updated_with_parent", JSONModelI18nWrapper.new(:digital_object_component => @digital_object_component, :digital_object => digital_object, :parent => parent).enable_parse_mixed_content!(url_for(:root))) :
-                      I18n.t("digital_object_component._frontend.messages.updated", JSONModelI18nWrapper.new(:digital_object_component => @digital_object_component, :digital_object => digital_object).enable_parse_mixed_content!(url_for(:root)))
 
+                    flash.now[:success] = parent ?
+                      t("digital_object_component._frontend.messages.updated_with_parent", digital_object_component_display_string: @digital_object_component.title) :
+                      t("digital_object_component._frontend.messages.updated", digital_object_component_display_string: @digital_object_component.title)
                     if @digital_object_component["is_slug_auto"] == false &&
                       @digital_object_component["slug"] == nil &&
                       params["digital_object_component"] &&
                       params["digital_object_component"]["is_slug_auto"] == "1"
 
-                      flash.now[:warning] = I18n.t("slug.autogen_disabled")
+                      flash.now[:warning] = t("slug.autogen_disabled")
                     end
 
                     render_aspace_partial :partial => "edit_inline"
@@ -58,6 +60,5 @@ Rails.application.config.after_initialize do
     end
 
   end
-  #from commit e66cd04
-
+  #from commit 0ff3a67
 end
